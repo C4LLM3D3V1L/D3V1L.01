4af4dbd74f2bb37b03cfa78bb7c944da79e577e3
D3V1L <96675285+C4LLM3D3V1L@users.noreply.github.com>
2021-12-26 13:21:31 +0600

    Add files via upload

---
+++ D3V1L_01.py
@@ -0,0 +1,37 @@
+import random
+import time
+blue= '\3[94m'
+lightblue= '\33[94m'
+red= '\33[91m'
+white= '\3[97m'
+yellow= '\3[93m'
+green= '\33[1;32m'
+cyan= '\33[0m'
+
+logo=red+str("""
+
+██████╗░██████╗░██╗░░░██╗░░███╗░░██╗░░░░░
+██╔══██╗╚════██╗██║░░░██║░████║░░██║░░░░░
+██║░░██║░█████╔╝╚██╗░██╔╝██╔██║░░██║░░░░░
+██║░░██║░╚═══██╗░╚████╔╝░╚═╝██║░░██║░░░░░
+██████╔╝██████╔╝░░╚██╔╝░░███████╗███████╗
+╚═════╝░╚═════╝░░░░╚═╝░░░╚══════╝╚══════╝
+
+IT'S NOT JUST A NAME, IT'S A BRAND
+──────────────────────────────────────────────────
+▸ AUTHOR     : Adnan Adif Hisham
+▸ FACEBOOK   : Facebook.com/call.me.devil.2004
+▸ GITHUB     : GITHUB.COM/C4LLM3D3V1L
+──────────────────────────────────────────────────
+
+""")
+
+print(logo)
+
+code = "1000"
+#ran_num = random.randint(11111111,99999999)
+while True:
+      ran_num = random.randint(11111111,99999999)
+      t_num = code+str(ran_num)
+      print(f"\33[1;32m[D3V1L-CP] {t_num} || 123456")
+      time.sleep(1)
